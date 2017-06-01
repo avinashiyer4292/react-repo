1a664e6a89dfb26bc29990d00569e0d51e04cf43
Avinash Iyer <avinashiyer4292@gmail.com>
2017-06-01 10:12:24 -0700

    Basic routing with react-router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import User from './components/pages/User';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import {Router} from 'react-router';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 import routes from './routes.js';
-import {createBrowserHistory} from 'history/createBrowserHistory';
-import {createHashHistory} from 'history/createHashHistory';
 
-const browserHistory = createBrowserHistory;
-var hashHistory   = createHashHistory;
+ReactDOM.render(
 
-ReactDOM.render(<Router history={hashHistory} routes={routes}/>, document.getElementById('root'));
+			<Router>
+				<div>
+					<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+						  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span className="navbar-toggler-icon"></span>
+						  </button>
+						  <a className="navbar-brand" href="#">Navbar</a>
+
+						  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+						    <ul className="navbar-nav mr-auto">
+						      <li className="nav-item active">
+						        <a className="nav-link" href="/">Home</a>
+						      </li>
+						      <li className="nav-item">
+						        <a className="nav-link" href="/user">User</a>
+						      </li>
+						    </ul>
+						    <form className="form-inline my-2 my-lg-0">
+						      <input className="form-control mr-sm-2" type="text" placeholder="Search"></input>
+						      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						    </form>
+						  </div>
+						</nav>
+					<Route path="/" component={App} />
+					<Route path="/user" component={User} />
+				</div>
+			</Router>
+
+	, document.getElementById('root'));
 registerServiceWorker();
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import {Route} from 'react-router';
 
 import App from './App';
 import About from './components/pages/About';
 import User from './components/pages/User';
 export default (
 		<Route name='homepage' component ={App}>
-			<Route path='/' name='Home' component={About} />
+			<Route path='/' name='Home' component={App} />
 			<Route path='/user' name='User' component={User} />
 
 		</Route>
