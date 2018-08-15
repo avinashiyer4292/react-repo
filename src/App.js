b4e7e93a725c2703f87b5206f1bf411934f83a50
avinashiyer <avinashiyer4292@gmail.com>
2018-08-14 21:32:35 -0700

    Print out each char as a separate component as we are inputting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import ValidationComponent from './react-16-assignment-2/components/ValidationComponent';
+import CharComponent from './react-16-assignment-2/components/CharComponent';
 class App extends Component {
 
   constructor(props){
     super();
     this.state ={
-      textLength: null
+      textLength: null,
+      text: ''
     }
   }
   showLength(event){
-      this.setState({textLength: event.target.value.length});
+      this.setState({textLength: event.target.value.length, text:event.target.value});
   }
   render() {
+
+    
+
     return (
       <div className="App">
         
         <input type="text" onChange={ (event) => this.showLength(event)} />
-        <p>Length: {this.state.text}</p>
+        <p>Length: {this.state.textLength}</p>
         <ValidationComponent length={this.state.textLength} />
+        {
+          Array.from(this.state.text.split(''))
+                .map( curr => {
+                  return <CharComponent char = {curr} />
+                })
+          }
+        
+        
       </div>
     );
   }
---
+++ src/react-16-assignment-2/components/CharComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+var CharComponent = (props) => {
+    return(
+        <p>{props.char}</p>
+    );
+}
+export default CharComponent;
